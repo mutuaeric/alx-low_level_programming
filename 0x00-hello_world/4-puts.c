e85adbfaad83994b58d324dd982320ae63a60b02
mutuaeric <ericmutua704@gmail.com>
2023-07-13 04:35:06 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * this program outputs the line Programming is like building a multilingual puzzle using the put function -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
