eadd179c72631e46c4cf6b421ec3f17458844030
mutuaeric <ericmutua704@gmail.com>
2023-07-13 04:51:05 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * this program outputs the line Programming is like building a multilingual puzzle using the put function -Entry point
+ * main -Entry point
  *
  * Return: Always 0 (Success)
  */
