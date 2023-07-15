9bbcc6fa784c04afc85267d393ee2d30e40b4c16
mutuaeric <ericmutua704@gmail.com>
2023-07-15 03:34:10 -0700

    variable-2

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	d = 0;
+	while
+		(d <= 9) {
+		       putchar(d + '0');
+		       d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
