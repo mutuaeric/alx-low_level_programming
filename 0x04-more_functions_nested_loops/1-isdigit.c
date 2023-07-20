61c121f4d2dc84618d30670ad2df9106bd171bb3
mutuaeric <ericmutua704@gmail.com>
2023-07-20 11:08:21 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - checks if is digit
+ * @c: value to be checked
+ * Return: Always 0
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - checks for multiplication values
+ * @a: a - first variable
+ * @b: a - second variable
+ * Return: Always  0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
