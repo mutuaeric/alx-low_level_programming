e42e81fa767f516192cb3051e471a0a9047cc22b
mutuaeric <ericmutua704@gmail.com>
2023-07-20 11:16:09 -0700

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - checks digit 0 to 9
+ *
+ * Return: Alway 0
+ */
+void print_numbers(void)
+{
+	int z;
+
+	for (z = 48; z < 58; z++)
+	{
+		_putchar(z);
+	}
+	_putchar('\n');
+}
