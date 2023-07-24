b46a80f2e35b38390dc70766abe87f65ac601a9e
mutuaeric <ericmutua704@gmail.com>
2023-07-24 07:45:27 -0700

    -8

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,27 +6,17 @@
  */
 int _atoi(char *s)
 {
-	int c = 0, m = 1, i = 0;
+	int c = 1;
 	unsigned int n = 0;
 
-	while (s[c])
-	{
-	if (s[c] == 45)
-	{
-		m *= -1;
-	}
-	while (s[c] >= 48 && s[c] <= 57)
-	{
-		i = 1;
-		n = (n * 10) + (s[c] - '\0');
-		c++;
-	}
-	if (i == 1)
-	{
-		break;
-	}
-	c++;
-	}
-	n *= m;
-	return (n);
+	do {
+		if (*s == '_')
+			c *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+
+	return (n * c);
 }
