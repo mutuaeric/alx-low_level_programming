c475d728f1fd5bae99325c8ad395b2ce4a3ce5ad
mutuaeric <ericmutua704@gmail.com>
2023-08-07 05:06:58 -0700

    rev3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,43 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-/**
- * _atoi - converts string to integer
- * @s: string
- * Return: int
- */
-int _atoi(char *s)
-{
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int l = 0;
-	int e = 0;
-	int di = 0;
-
-	while (s[l] != '\0')
-		l++;
-	while (a < l && e == 0)
-	{
-		if (s[a] == '-')
-			++b;
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			di = s[a] - '0';
-			if (b % 2)
-				di = -di;
-			c = c * 10 + di;
-			e = 1;
-			if (s[a + 1], '0' || s[a + 1] > '0')
-			break;
-		e = 0;
-		}
-		a++;
-	}
-		if (e == 0)
-
-		return (0);
-	return (c);
-}
 /**
  * main - multiplies 2 inputs
  * @argc: number of arguments
@@ -46,17 +9,19 @@ int _atoi(char *s)
  */
 int main(int argc, char *argv[])
 {
-	int output, x, z;
+	int output, x = 0, z = 0;
 
-	x = _atoi(argv[1]);
-	z = _atoi(argv[2]);
-	output = x * z;
+	if (argc == 3)
+	{
+		x = _atoi(argv[1]);
+		z = _atoi(argv[2]);
+		output = x * z;
 	printf("%d\n", output);
-	return (0);
-
-	if (argc < 3 || argc > 3)
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
+	return (0);
+}
