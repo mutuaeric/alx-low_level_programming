022f2dfde682a1ae635398714b881b5ed2d79445
mutuaeric <ericmutua704@gmail.com>
2023-08-07 08:45:48 -0700

    6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -48,6 +48,7 @@ char **strtow(char *str)
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
 			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
 			j++;
 			w[wc] = (char *)malloc(j * sizeof(char));
 			j--;
