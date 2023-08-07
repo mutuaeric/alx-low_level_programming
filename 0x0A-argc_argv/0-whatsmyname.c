5a0d0814f5a3ffca2b6aa88245b65c27ae28db85
mutuaeric <ericmutua704@gmail.com>
2023-08-07 02:18:54 -0700

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints program name
+ * @argc: number of arguments
+ * @argv: arrey of arguments
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory is about C arguments and how they are applied in C programming
